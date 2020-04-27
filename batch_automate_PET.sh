0f515fe1e362ba2ac73cf7b519ec68a49a5eb365
Rebekah Wickens <rwickens@gmail.com>
2020-04-27 16:58:12 -0400

    Add files via upload
    Mini shell script to run automate_PET.py on all patient folders in a project folder. 
    Requires personalization and silencing of minc "register" command which causes pop-up.

---
+++ batch_automate_PET.sh
@@ -0,0 +1,8 @@
+# In order to run: 
+# Suppress the register commands in steps 8A and *B
+# Change the "full/path_to/project_folder/*/ and the 
+# full/path_to/automate_PET.py to match your settings. 
+
+for i in /full/path_to/project_folder/*/  ; 
+do python /full/path_to/automate_PET.py $i; 
+done
