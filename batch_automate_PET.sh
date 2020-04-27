a140428dcd82d99adee7f04dffbac52eef66136f
Rebekah Wickens <rwickens@gmail.com>
2020-04-27 17:02:35 -0400

    Update batch_automate_PET.sh

--- batch_automate_PET.sh
+++ batch_automate_PET.sh
@@ -1,9 +1,10 @@
 #!bin/bash
 
 # In order to run: 
-# Suppress the register commands in steps 8A and *B
-# Change the "full/path_to/project_folder/*/ and the 
-# full/path_to/automate_PET.py to match your settings. 
+# Suppress the register commands in steps 8A and 8B
+# Customize the "full/path_to/project_folder/*/ and the 
+# full/path_to/automate_PET.py to match your settings.
+# This can be easily be done on terminal. 
 
 for i in /full/path_to/project_folder/*/  ; 
 do python /full/path_to/automate_PET.py $i; 
